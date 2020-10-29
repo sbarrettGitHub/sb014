11e0ea92e267f2b761c77d2d3adcde5516d10bdf
Sbarrett <barrettseamus@hotmail.com>
2020-10-29 18:01:13 +0000

    Add unsubscribe signalr action

--- SB014.API/Notifications/TournamentNotificationHub.cs
+++ SB014.API/Notifications/TournamentNotificationHub.cs
@@ -8,8 +8,11 @@ namespace SB014.API.Notifications
     {       
         public async Task SubscribeToTournamentNotifications(Guid tournamentId)
         {
-
             await Groups.AddToGroupAsync(Context.ConnectionId, tournamentId.ToString());
+        }
+        public async Task UnsubscribeFromTournamentNotifications(Guid tournamentId)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, tournamentId.ToString());
         }
     }
 }
